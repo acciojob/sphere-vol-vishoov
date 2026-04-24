f069d3f24946ecc35568fecdab7083b6d11783db
Vishoo Verma <78697370+vishoov@users.noreply.github.com>
2026-04-24 13:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+  const radius = document.getElementById('radius').value;
+const radiusValue = parseFloat(radius);
+	const output = document.getElementById('volume');
+
+	if(isNaN(radiusValue) || radius<0){
+		output.value="NaN"
+	}else{
+		const volume = (4/3)*Math.PI*Math.pow(radiusValue, 3);
+		output.value = volume;
+	}
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
